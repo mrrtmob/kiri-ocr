d6e4cd18d5c5b5b1dd06f368828776f6fc78d43c
Tmob <mrrtmob@gmail.com>
2026-01-16 21:13:42 +0700

    update pypi

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="kiri_ocr",
-    version="0.1.1",
-    description="A lightweight OCR library for Khmer and English documents",
-    author="Blizzer",
-    packages=find_packages(),
-    install_requires=[
-        "torch",
-        "numpy",
-        "opencv-python",
-        "Pillow",
-        "tqdm",
-        "PyYAML",
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache-2.0",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-    include_package_data=True,
-    scripts=['scripts/kiri-ocr'],
-)
+setup()
