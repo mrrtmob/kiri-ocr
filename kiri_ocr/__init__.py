c7876391a526700980a05d3be154ddfd9e939b6b
Tmob <mrrtmob@gmail.com>
2026-01-27 19:56:03 +0700

    Bump version to 0.2.3

--- kiri_ocr/__init__.py
+++ kiri_ocr/__init__.py
@@ -11,7 +11,7 @@ from .renderer import DocumentRenderer
 from .model import KiriOCR, CFG, CharTokenizer
 from .detector import TextDetector
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 __all__ = [
     'OCR',
